e9c7ba251cb3ff6c056284fc702545917f6f495a
Wouter de Bruijn <wouter@hedium.nl>
2022-08-09 15:22:00 +0200

    🍱 Added small icon set

---
+++ components/IconOpenFull.tsx
@@ -0,0 +1,17 @@
+/** @jsx h */
+import { h } from "preact";
+import { tw } from "../utils/twind.ts";
+
+export default function IconOpenFull({ size = 16 }: { size?: number }) {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      viewBox="0 0 48 48"
+      height={size}
+      width={size}
+      class={tw`inline`}
+    >
+      <path d="M6 42V27h3v9.9L36.9 9H27V6h15v15h-3v-9.9L11.1 39H21v3Z" />
+    </svg>
+  );
+}
