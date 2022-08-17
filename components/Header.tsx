9c313d82443b2fc4e1eeeb0dd87b236688ecd588
Wouter de Bruijn <wouter@hedium.nl>
2022-08-17 15:28:08 +0200

    :sparkles: Added home href to header

--- components/Header.tsx
+++ components/Header.tsx
@@ -4,13 +4,15 @@ import { tw } from "@twind";
 
 export default function Header() {
   return (
-    <div class={tw`h-20 bg-yellow-400 flex items-center`}>
-      <img
-        src="/logo.png"
-        alt="the fresh logo: a sliced lemon dripping with juice"
-        class={tw`h-full p-1`}
-      />
-      <h1 class={tw`text-xl pl-2`}>Docker Compose Management</h1>
-    </div>
+    <header class={tw`bg-yellow-400 flex items-center`}>
+      <a href="/">
+        <img
+          src="/logo.png"
+          alt="the fresh logo: a sliced lemon dripping with juice"
+          class={tw`h-20 p-2`}
+        />
+      </a>
+        <h1 class={tw`text-xl pl-2`}>Docker Compose Management</h1>
+    </header>
   );
 }
